867ab3a60fa9c0d0a120d23fbc8aac80c0d398d0
wangyuzhuo1 <113219030+wangyuzhuo1@users.noreply.github.com>
2023-10-29 12:39:29 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 //https://teachablemachine.withgoogle.com/models/3SlSP2PiC/
+
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({audio:true})
+    classifier=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/3SlSP2PiC/.model.json", modelready)
+}
+function modelready(){
+    classifier.classify(got_results())
+}
